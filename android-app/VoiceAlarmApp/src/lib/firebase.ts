2329234676c07a3c3a6b51df998fa26c202df00c
Tarmo Lehto <tarmoejlehto@gmail.com>
2025-09-25 19:40:49 +0300

    Removed obsolete firebase.ts file. This version works.

--- android-app/VoiceAlarmApp/src/lib/firebase.ts
+++
@@ -1,5 +0,0 @@
-// src/lib/firebase.ts
-import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
-import firestore from '@react-native-firebase/firestore';
-
-export { auth, firestore, FirebaseAuthTypes };
